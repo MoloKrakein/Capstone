a67cfd685fd7350be74cc4991d41c89418968e80
MoloKrakein <51546600+MoloKrakein@users.noreply.github.com>
2023-12-18 14:54:17 +0700

    Add canvas and text for story and implement scene transition

--- Assets/sceneTrigger.cs
+++ Assets/sceneTrigger.cs
@@ -2,15 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class sceneTrigger : MonoBehaviour
 { // Nama scene yang akan dipindahkan
     public string targetSceneName;
+    public Canvas canvasStart;
+    public Canvas canvasStory;
+    public TextMeshProUGUI textStory;
+    // text area for story
+    [TextArea(10,10)]
+    public string story;
 
-    // Method ini akan dipanggil ketika objek diklik
-    private void OnMouseDown()
+    // change scene
+    public void changeScene()
     {
-        // Pindah ke scene lain
+        // play canvasStartAnimation
+        canvasStart.GetComponent<Animator>().SetTrigger("start");
+        // set canvasStory to active
+        canvasStory.gameObject.SetActive(true);
+    }
+
+    public void PlayStory(string story,TextMeshProUGUI textStory)
+    {
+        StartCoroutine(PlayText(story,textStory));
+        // load scene
+        StartCoroutine(LoadScene());
+    }
+
+    IEnumerator PlayText(string story,TextMeshProUGUI textStory)
+    {
+        textStory.text = "";
+        foreach (char c in story)
+        {
+            textStory.text += c;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    IEnumerator LoadScene()
+    {
+        yield return new WaitForSeconds(5f);
         SceneManager.LoadScene(targetSceneName);
     }
 }
